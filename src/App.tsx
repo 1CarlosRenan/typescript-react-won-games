362a59e25c348000b718d568e5a466b952270961
1carlosrenan <carlosrenan96@gmail.com>
2023-03-14 23:55:16 -0300

    feat: adding global style with local fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes'
+import GlobalSyles from './styles/global'
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalSyles />
       <AppRoutes />
     </BrowserRouter>
   )
